4cb2e938b848e50e09ba65768477795592575c29
yeltag <ytaghiyeva@gmail.com>
2026-07-13 15:51:15 +0400

    The main app is ready, pages index and about

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     context: dict[str,str]={
-        'title':"Home",
-        'content':'Главная страница магазина - HOME',
-        'list':['first','second'],
-        'dict':{'first':1},
-        'is_autenticated': False
+        'title':"Home - Главная ",
+        'content':'Магазин мебели HOME'
     }
     
     return render(request,'main/index.html',context)
 
 def about(request):
-    return HttpResponse("About page")
+    context: dict[str,str]={
+        'title':"Home - О нас",
+        'content':'О нас',
+        'text_on_page': "Текст о том почему этот магазин такой классный."
+    }
+    
+    return render(request,'main/about.html',context)
