ebfea0ea3721cb710bd34823d67b6bdd8d061773
yeltag <ytaghiyeva@gmail.com>
2026-07-15 22:23:52 +0400

    added goods app

--- app/urls.py
+++ app/urls.py
@@ -19,6 +19,15 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace = 'main'))
+    path('', include('main.urls', namespace = 'main')),
+    path('catalog/', include('goods.urls', namespace = 'catalog'))
     
 ]
+
+"""
+www.site.com/admin/
+www.site.com
+www.site.com/about/
+www.site.com/catalog/
+www.site.com/catalog/product
+"""
