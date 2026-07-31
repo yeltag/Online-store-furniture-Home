ff8770f6c16a6cb1e8c4e3e82e3555605bd4df92
yeltag <ytaghiyeva@gmail.com>
2026-07-31 16:36:42 +0400

    Second pagination option - GET params

--- goods/views.py
+++ goods/views.py
@@ -4,7 +4,11 @@ from goods.models import Products
 
 from django.core.paginator import Paginator
 
-def catalog(request,category_slug,page = 1):
+def catalog(request,category_slug):
+
+    page = request.GET.get('page',1)
+
+
     if category_slug == "all":
     
         goods = Products.objects.all()
@@ -13,7 +17,7 @@ def catalog(request,category_slug,page = 1):
         
     paginator = Paginator(goods, 3)
 
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context  = {
         "title": "Home - Каталог",
