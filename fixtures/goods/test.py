6fe0360eabd5b6ab50df0b868712583479b3cd27
yeltag <ytaghiyeva@gmail.com>
2026-08-04 15:33:03 +0400

    Simple search created

--- fixtures/goods/test.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open(r'c:\Users\TaghiyevaYel\VisualStudio projects\app1\fixtures\goods\cats.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-    for item in data:
-        print(item['fields']['name'])  # Should show Russian text
