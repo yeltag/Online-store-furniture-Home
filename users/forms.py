cd2a97c0929b6af3b3d17088b746296c5997b3ce
yeltag <ytaghiyeva@gmail.com>
2026-08-17 15:26:12 +0400

    profile form controller and notifications are ready

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -57,4 +57,59 @@ class UserRegistrationForm(UserCreationForm):
                    
     #           }
     #      )
+    # )
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields=(
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",)
+        
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()    
+        
+
+        
+    # image = forms.ImageField(
+    #     widget=forms.FileInput(attrs={"class":"form_control mt-3"}), required=False
+    # )
+    # first_name=forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class":"form_control",
+    #             "placeholder": "Введите ваше имя",
+    #         }
+    #     )
+    # )
+    # last_name = forms.CharField(
+    #     widget = forms.TextInput(
+    #         attrs={
+    #             "class":"form-control",
+    #             "placeholder":"Введите вашу фамилию",
+    #         }
+    #     )
+    # )
+    # username = forms.Charfield(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class":"form-control",
+    #             "placeholder":"Введите ваше имя пользователя",
+    #         }
+    #     )
+    # )
+    # email = forms.Charfield(
+    #     widget=forms.EmailInput(
+    #         attrs={
+    #             "class":"form-control",
+    #             "placeholder":"Введите ваш email *youremail@example.com",
+    #         }
+    #     )
     # )
--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserRegistrationForm, UserLoginForm
-
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 
 # Create your views here.
@@ -18,6 +18,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
                 return HttpResponseRedirect(reverse('main:index'))
     else:       
         form = UserLoginForm()
@@ -35,6 +36,7 @@ def registration(request):
                 form.save()
                 user=form.instance
                 auth.login(request,user)
+                messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
 
                 return HttpResponseRedirect(reverse('main:index'))
     else:       
@@ -46,12 +48,25 @@ def registration(request):
     }
     return render(request, 'users/registration.html', context)
 
+@ login_required
 def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+                form.save()
+                messages.success(request, "Профайл успешно обговлен")
+                return HttpResponseRedirect(reverse('user:profile'))
+    else:       
+        form = ProfileForm(instance=request.user)
+    
+    
     context = {
-        'title': "Home - Кабинет"
+        'title': "Home - Кабинет",
+        'form': form
     }
     return render(request, 'users/profile.html', context)
 
 def logout(request):
+    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
     return redirect(reverse("main:index"))
